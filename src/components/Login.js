1c441988bf784c157f70cc6b9c386b0e41368f29
Kundan Kumar <kksr1996@gmail.com>
2024-03-23 23:35:25 +0530

    adding form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
 import poster from "../resources/netflixposter.jpg";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setSignIn(!isSignIn);
+  };
+
+  const handleButtonClick = () => {
+    // Validate the form data on this button click
+    const msg = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(msg);
   };
 
   return (
@@ -15,28 +25,37 @@ const Login = () => {
       <div className="absolute">
         <img src={poster} alt="poster" />
       </div>
-      <form className="absolute p-16 bg-black w-[20%] m-auto left-[40%] top-[10%] bg-opacity-75 rounded-lg">
+      <form
+        className="absolute p-16 bg-black w-[20%] m-auto left-[40%] top-[10%] bg-opacity-75 rounded-lg"
+        onClick={(e) => e.preventDefault()}
+      >
         <div className="font-extrabold text-3xl text-white p-2 font-sans">
           Sign In
         </div>
         <input
-          type="text"
+          type="email"
           placeholder="Email Address"
-          className="py-3 px-4 m-2 my-6 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md"
+          ref={email}
+          className="py-3 px-4 m-2 my-6 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md text-white"
         />
         {!isSignIn && (
           <input
             type="text"
             placeholder="Enter Name"
-            className="py-3 px-4 m-2 my-6 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md"
+            className="py-3 px-4 m-2 my-6 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md text-white"
           />
         )}
         <input
           type="password"
           placeholder="Enter Password"
-          className="py-3 px-4 m-2 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md"
+          ref={password}
+          className="py-3 px-4 m-2 block w-full bg-gray-800 bg-transparent bg-opacity-80 rounded-md text-white"
         />
-        <button className="px-4 py-2 m-2 mt-8 rounded-sm text-white bg-red-600 w-full cursor-pointer">
+        <p className="text-red-600 xl mx-4 mt-4">{errorMessage}</p>
+        <button
+          className="px-4 py-2 m-2 mt-7 rounded-sm text-white bg-red-600 w-full cursor-pointer"
+          onClick={handleButtonClick}
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <div className="text-white mx-2 my-6">
@@ -45,7 +64,7 @@ const Login = () => {
               New to Netflix?
               <span
                 onClick={toggleSignInForm}
-                className="font-bold cursor-pointer"
+                className="font-bold cursor-pointer lg"
               >
                 {" "}
                 Sign Up Now
@@ -56,7 +75,7 @@ const Login = () => {
               Already Registered!
               <span
                 onClick={toggleSignInForm}
-                className="font-bold cursor-pointer"
+                className="font-bold cursor-pointer lg"
               >
                 {" "}
                 Sign In Now
