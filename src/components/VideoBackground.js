ae80cc21d1dbfcabb44fc89d3b07a23175ba60dd
Kundan Kumar <kksr1996@gmail.com>
2024-04-01 10:41:08 +0530

    putting the youtube trailer in the background with autoplay feature

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,9 +11,12 @@ const VideoBackground = ({ movie_id }) => {
     <div>
       {trailer && (
         <iframe
-          width="560"
-          height="315"
-          src={"https://www.youtube.com/embed/" + trailer.key}
+          className="w-screen aspect-video"
+          src={
+            "https://www.youtube.com/embed/" +
+            trailer.key +
+            "?&autoplay=1&mute=1"
+          }
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,14 +3,14 @@ import React from "react";
 const VideoTitle = ({ title, overview, movie_id }) => {
   console.log(title + ": " + movie_id);
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video absolute pt-[20%] px-24 text-white bg-gradient-to-r from-gray-800">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/2">{overview}</p>
       <div>
-        <button className="px-12 py-3 bg-white text-black text-lg rounded-lg bg-opacity-50 shadow-md">
+        <button className="px-12 py-3 bg-white text-black text-lg rounded-lg shadow-md hover:bg-opacity-80">
           ▶️ Play
         </button>
-        <button className="px-8 py-3 mx-2 bg-white text-black text-lg rounded-lg bg-opacity-50 shadow-md">
+        <button className="px-8 py-3 mx-2 bg-gray-500 text-white text-lg rounded-lg shadow-md hover:bg-opacity-80">
           ℹ️ More Info
         </button>
       </div>
